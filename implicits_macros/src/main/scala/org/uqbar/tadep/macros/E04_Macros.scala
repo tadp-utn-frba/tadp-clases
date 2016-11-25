c9133925a3e46ae87f3cedaf909a48c2b3cf040f
Ernesto Bossi <bossi.ernestog@gmail.com>
2016-11-25 18:27:50 -0300

    Add some more quasiquotes examples

--- implicits_macros/src/main/scala/org/uqbar/tadep/macros/E04_Macros.scala
+++ implicits_macros/src/main/scala/org/uqbar/tadep/macros/E04_Macros.scala
@@ -2,101 +2,123 @@ package org.uqbar.tadep.macros
 
 import scala.language.experimental.macros
 import scala.reflect.macros.blackbox.Context
+import scala.reflect.runtime.universe.showRaw //for getting the showRaw
+
 
 object E04_Macros {
 
-	//═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════
-	// Ejemplo fácil
-	//═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════
+  //═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════
+  // Ejemplo fácil
+  //═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════
 
-	def id(n: Int): Int = macro id_impl
+  def id(n: Int): Int = macro id_impl
 
-	def id_impl(c: Context)(n: c.Expr[Int]): c.Expr[Int] = {
-		n
-	}
+  def id_impl(c: Context)(n: c.Expr[Int]): c.Expr[Int] = {
+    n
+  }
 
-	//═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════
-	// Ejemplo con Quasiquotes
-	//═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════
-	def assert(contidion: Boolean, msg: String): Unit = macro assert_impl
-	
-	def assert_impl(c: Context)(contidion: c.Expr[Boolean], msg: c.Expr[String]) = {
-	  import c.universe._
-	  
-	  val q"assert ($condition, $msg)" = c.macroApplication
-	  q"if (!$condition) raise($msg)"
-	}
-	
-	
-	def debug(code: => Unit): Unit = macro debug_impl
+  //═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════
+  // Ejemplo con Quasiquotes
+  //═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════
+  def assert(contidion: Boolean, msg: String): Unit = macro assert_impl
 
-	def debug_impl(c: Context)(code: c.Tree) = {
-		import c.universe._
+  def assert_impl(c: Context)(contidion: c.Expr[Boolean], msg: c.Expr[String]) = {
+    import c.universe._
 
-		val q"..$sentences" = code
+    val q"assert ($condition, $msg)" = c.macroApplication
+    q"if (!$condition) raise($msg)"
+  }
 
-		val loggedSentences = (sentences :\ List[c.Tree]()){
-			case (sentence, acum) =>
-				val msg = "executing " + showCode(sentence)
-				val printSentence = q"println($msg)"
+  def getValMacro(c: Context)(code: c.Expr[Any]): c.Expr[Any] = {
+    import c.universe._
+    val q"val $name = $value" = code.tree match { case Block(List(valdef), _) => valdef }
+    c.Expr(value)
+  }
 
-				printSentence :: sentence :: acum
-		}
+  def getVal(code: Any) = macro getValMacro
 
-		q"..$loggedSentences"
-	}
+  //getVal{val a = "Bleh"}
+  //ejecutar en otro contexto de ejecucion
+  //  getVal{
+  //    val a = "Bleh"
+  //    val b = 1
+  //  }
 
-	//═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════
-	// Ejemplo con validación
-	//═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════
+  def printTreeMacro(c: Context)(code: c.Expr[Any]): c.Expr[Any] = {
+    println(showRaw(code.tree))
+    c.universe.reify { () }
+  }
+  def printTree(code: Any) = macro printTreeMacro
 
-	case class Email(id: String, domain: String)
+  def debug(code: => Unit): Unit = macro debug_impl
 
-	def email(str: String): Email = macro email_impl
-	def email_impl(c: Context)(str: c.Expr[String]) = {
-		import c.universe._
+  def debug_impl(c: Context)(code: c.Tree) = {
+    import c.universe._
 
-		val emailFormat = """(\w{4,})@([\w\.]+.com)""".r
-		
-		str match {
-			case Expr(Literal(Constant(emailFormat(id, domain)))) =>	q"""Email($id,$domain)"""
-			case _ => c.abort(c.enclosingPosition, "Invalid mail!!!")
-		}
-	}
-	
-	//═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════
-	// Poniendo todo junto
-	//═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════
+    val q"..$sentences" = code
 
-//	case class Email(id: String, domain: String)
-//
-//	implicit class EmailStringContext(strCtx: StringContext) {
-//		def email(arguments: Any*): Email = macro email_impl
-//	}
-//	
-//	def email_impl(c: Context)(arguments: c.Expr[Any]*) = {
-//		import c.universe._
-//
-//		val emailFormat = """(\w{4,})@([\w\.]+.com)""".r
-//		
-//		c.prefix.tree match {
-//      case Apply(_, List(Apply(_, rawParts))) =>
-//      	
-//      	val parts = rawParts map { case Literal(Constant(const: String)) => const }
-//      	val args = arguments map { case Literal(Constant(const: String)) => const }
-//      	val mail = ("" /: parts.zipAll(args, "", "")) {	case (acum, (part, arg)) => acum + part + arg	}
-//
-//      	mail match {
-//      		case emailFormat(id,domain) => q"Email($id,$domain)"
-//      		case _ => c.abort(c.enclosingPosition, "Invalid mail!!!")
-//      	}
-//      	
-//			case _ => c.abort(c.enclosingPosition, "Invalid mail!!!")
-//		}
-//	}
+    val loggedSentences = (sentences :\ List[c.Tree]()) {
+      case (sentence, acum) =>
+        val msg = "executing " + showCode(sentence)
+        val printSentence = q"println($msg)"
+
+        printSentence :: sentence :: acum
+    }
+
+    q"..$loggedSentences"
+  }
+
+  //═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════
+  // Ejemplo con validación
+  //═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════
+
+  case class Email(id: String, domain: String)
+
+  def email(str: String): Email = macro email_impl
+  def email_impl(c: Context)(str: c.Expr[String]) = {
+    import c.universe._
+
+    val emailFormat = """(\w{4,})@([\w\.]+.com)""".r
+
+    str match {
+      case Expr(Literal(Constant(emailFormat(id, domain)))) => q"""Email($id,$domain)"""
+      case _ => c.abort(c.enclosingPosition, "Invalid mail!!!")
+    }
+  }
+
+  //═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════
+  // Poniendo todo junto
+  //═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════
+
+  //	case class Email(id: String, domain: String)
+  //
+  //	implicit class EmailStringContext(strCtx: StringContext) {
+  //		def email(arguments: Any*): Email = macro email_impl
+  //	}
+  //	
+  //	def email_impl(c: Context)(arguments: c.Expr[Any]*) = {
+  //		import c.universe._
+  //
+  //		val emailFormat = """(\w{4,})@([\w\.]+.com)""".r
+  //		
+  //		c.prefix.tree match {
+  //      case Apply(_, List(Apply(_, rawParts))) =>
+  //      	
+  //      	val parts = rawParts map { case Literal(Constant(const: String)) => const }
+  //      	val args = arguments map { case Literal(Constant(const: String)) => const }
+  //      	val mail = ("" /: parts.zipAll(args, "", "")) {	case (acum, (part, arg)) => acum + part + arg	}
+  //
+  //      	mail match {
+  //      		case emailFormat(id,domain) => q"Email($id,$domain)"
+  //      		case _ => c.abort(c.enclosingPosition, "Invalid mail!!!")
+  //      	}
+  //      	
+  //			case _ => c.abort(c.enclosingPosition, "Invalid mail!!!")
+  //		}
+  //	}
 }
 
 object USO {
-	import E04_Macros._
+  import E04_Macros._
 
 }
