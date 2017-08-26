bcaa45e4801b4c11e9bfe79844ae1b1e287d8f09
pablitar <pablitar@gmail.com>
2017-08-26 12:33:23 -0300

    Temas de clase 2

--- spec/age_spec.rb
+++ spec/age_spec.rb
@@ -45,105 +45,141 @@ describe 'age of empires tests' do
     don_quijote = Espadachin.new(Espada.new(40))
     #Esto es la manera sintáctica de decir que lo que se espera dentro de los {} del expect, lanza una exception.
     #Despues veremos que quieren decir los {}
-    expect { muralla.atacar don_quijote }.to raise_error(NoMethodError)
+    expect {muralla.atacar don_quijote}.to raise_error(NoMethodError)
   end
 
   it 'Misil no defiende' do
     misil = Misil.new
     don_quijote = Espadachin.new(Espada.new(40))
-    expect { don_quijote.atacar misil }.to raise_error(NoMethodError)
+    expect {don_quijote.atacar misil}.to raise_error(NoMethodError)
   end
 
   ######################
 
-  # it 'Atacante descansado pega doble' do
-  #   atila = Guerrero.new #(potencial_ofensivo = 20, energia = 100, potencial_defensivo = 10)
-  #   conan = Guerrero.new
+  it 'Atacante descansado pega doble' do
+    atila = Guerrero.new #(potencial_ofensivo = 20, energia = 100, potencial_defensivo = 10)
+    conan = Guerrero.new
+
+    atila.descansar
+    atila.atacar conan
+
+    # 100 - (20 * 2 - 10)
+    expect(conan.energia).to eq(70)
+  end
   #
-  #   atila.descansar
-  #   atila.atacar conan
+  it 'Atacante descansado ataca doble solo una vez por descanso' do
+    atila = Guerrero.new
+    conan = Guerrero.new
+    heman = Guerrero.new
+
+    atila.descansar
+    atila.atacar conan
+    atila.atacar heman
+
+    # 100 - (20 - 10)
+    expect(heman.energia).to eq(90)
+  end
+
+  it 'Un misil descansado tiene el doble de potencial ofensivo' do
+    unMisil = Misil.new
+
+    unMisil.descansar
+
+    expect(unMisil.potencial_ofensivo).to eq(2000)
+  end
   #
-  #   # 100 - (20 * 2 - 10)
-  #   expect(conan.energia).to eq(70)
-  # end
+  it 'Defensor descansado suma 10' do
+    muralla = Muralla.new
+    expect(muralla.energia).to eq(200)
+
+    muralla.descansar
+
+    expect(muralla.energia).to eq(210)
+  end
   #
-  # it 'Atacante descansado ataca doble solo una vez por descanso' do
-  #   atila = Guerrero.new
-  #   conan = Guerrero.new
-  #   heman = Guerrero.new
+  it 'Guerrero descansa como Defensor y como Atacante' do
+    atila = Guerrero.new #(potencial_ofensivo = 20, energia = 100, potencial_defensivo = 10)
+    conan = Guerrero.new
+    expect(atila.energia).to eq(100)
+
+    atila.descansar
+    atila.atacar conan
+
+    expect(conan.energia).to eq(70)
+    expect(atila.energia).to eq(110)
+  end
   #
-  #   atila.descansar
-  #   atila.atacar conan
-  #   atila.atacar heman
+  it 'kamikaze pierde su energia luego de atacar' do
+    kamikaze = Kamikaze.new #(potencial_ofensivo = 250, energia = 100, potencial_defensivo = 10)
+    muralla = Muralla.new #(potencial_defensivo = 50, energia = 200)
+
+    kamikaze.atacar(muralla)
+
+    expect(muralla.energia).to eq(0)
+    expect(kamikaze.energia).to eq(0)
+  end
   #
-  #   # 100 - (20 - 10)
-  #   expect(heman.energia).to eq(90)
-  # end
-  #
-  # it 'Defensor descansado suma 10' do
-  #   muralla = Muralla.new
-  #   expect(muralla.energia).to eq(200)
-  #
-  #   muralla.descansar
-  #
-  #   expect(muralla.energia).to eq(210)
-  # end
-  #
-  # it 'Guerrero descansa como Defensor y como Atacante' do
-  #   atila = Guerrero.new #(potencial_ofensivo = 20, energia = 100, potencial_defensivo = 10)
-  #   conan = Guerrero.new
-  #   expect(atila.energia).to eq(100)
-  #
-  #   atila.descansar
-  #   atila.atacar conan
-  #
-  #   expect(conan.energia).to eq(70)
-  #   expect(atila.energia).to eq(110)
-  # end
-  #
-  # it 'kamikaze pierde su energia luego de atacar' do
-  #   kamikaze = Kamikaze.new #(potencial_ofensivo = 250, energia = 100, potencial_defensivo = 10)
-  #   muralla = Muralla.new #(potencial_defensivo = 50, energia = 200)
-  #
-  #   kamikaze.atacar(muralla)
-  #
-  #   expect(muralla.energia).to eq(0)
-  #   expect(kamikaze.energia).to eq(0)
-  # end
-  #
-  # it 'kamikaze descansa solo como atacante' do
-  #   kamikaze = Kamikaze.new
-  #
-  #   expect(kamikaze.potencial_ofensivo).to eq(250)
-  #
-  #   kamikaze.descansar
-  #
-  #   expect(kamikaze.energia).to eq(100)
-  #   expect(kamikaze.potencial_ofensivo).to eq(500)
-  # end
+  it 'kamikaze descansa solo como atacante' do
+    kamikaze = Kamikaze.new
+
+    expect(kamikaze.potencial_ofensivo).to eq(250)
+
+    kamikaze.descansar
+
+    expect(kamikaze.energia).to eq(100)
+    expect(kamikaze.potencial_ofensivo).to eq(500)
+  end
   #
   # ######################
   #
-  # it('Peloton descansador hace descansar a sus guerreros que no estan descansados') do
-  #   atila = Guerrero.new
-  #   vikingo = Guerrero.new 70
-  #   don_quijote = Espadachin.new(Espada.new(50))
-  #   Peloton.descansador([atila, vikingo])
+  it('Peloton descansador hace descansar a sus guerreros que no estan descansados') do
+    atila = Guerrero.new
+    vikingo = Guerrero.new 100, 70
+    don_quijote = Espadachin.new(Espada.new(50))
+    Peloton.descansador(atila, vikingo)
+
+    don_quijote.atacar(vikingo)
+    #Al ser atacado, el vikingo le avisa al ejercito. El vikingo no esta descansado luego de recibir el ataque
+    expect(atila.energia).to eq(100) #Atila no descansa, porque esta descansado
+    expect(vikingo.energia).to eq(50) #En un test anterior energia quedaba en 40, pero ahora como descanso queda en 50
+    expect(vikingo.potencial_ofensivo).to eq(140) #Ademas, su potencial_ofensivo se duplica (para el proximo ataque)
+  end
   #
-  #   don_quijote.atacar(vikingo)
-  #   #Al ser atacado, el vikingo le avisa al ejercito. El vikingo no esta descansado luego de recibir el ataque
-  #   expect(atila.energia).to eq(100) #Atila no descansa, porque esta descansado
-  #   expect(vikingo.energia).to eq(50) #En un test anterior energia quedaba en 40, pero ahora como descanso queda en 50
-  #   expect(vikingo.potencial_ofensivo).to eq(140) #Ademas, su potencial_ofensivo se duplica (para el proximo ataque)
-  # end
-  #
-  # it 'Peloton cobarde se retira cuando sufre danio uno de sus guerreros' do
-  #   atila = Guerrero.new
-  #   vikingo = Guerrero.new 70
-  #   peloton = Peloton.cobarde([atila])
-  #
-  #   vikingo.atacar(atila)
-  #
-  #   expect(peloton.retirado).to be(true)
-  # end
+  it 'Peloton cobarde se retira cuando sufre danio uno de sus guerreros' do
+    atila = Guerrero.new
+    vikingo = Guerrero.new 70
+    peloton = Peloton.cobarde(atila)
+
+    vikingo.atacar(atila)
+
+    expect(peloton.retirado).to be(true)
+  end
+
+  it 'Peloton estrategico' do
+    atila = Guerrero.new
+    vikingo = Guerrero.new 100, 70
+    peloton = Peloton.estrategico(atila)
+
+    vikingo.atacar(atila)
+
+    expect(peloton.retirado).to be(true)
+    expect(atila.potencial_ofensivo).to eq(20)
+    peloton.retirado = false
+    vikingo.atacar(atila)
+    expect(atila.potencial_ofensivo).to eq(40)
+    expect(peloton.retirado).to eq(false)
+  end
+
+  it 'Peloton patotero' do
+    atila = Guerrero.new
+    vikingo = Guerrero.new 100, 70
+    don_quijote = Espadachin.new(Espada.new(50))
+
+    giordano = Guerrero.new
+    peloton = Peloton.patotero(giordano, [atila, vikingo])
+
+    don_quijote.atacar(atila)
+
+    expect(giordano.energia).to be(30)
+  end
 end
--- src/age.rb
+++ src/age.rb
@@ -1,30 +1,85 @@
 module UnidadDefensiva
 
-  attr_accessor :energia, :potencial_defensivo
+  attr_accessor :potencial_defensivo
 
   def ser_atacado(potencial_ofensivo)
     self.energia = self.energia - (potencial_ofensivo - self.potencial_defensivo)
   end
 
+  def descansar
+    self.energia += 10
+  end
+
+  def lastimado
+    self.energia <= 40
+  end
+
+  def energia
+    @energia ||= 100
+  end
+
+  def energia=(una_energia)
+    if una_energia > 0
+      @energia = una_energia
+    else
+      @energia = 0
+    end
+  end
+
 end
 
 module UnidadOfensiva
+  attr_accessor :descansado
+
   def atacar(otro_guerrero)
     otro_guerrero.ser_atacado(self.potencial_ofensivo)
+    self.descansado = false
+  end
+
+  def potencial_ofensivo
+    if(self.descansado)
+      self.potencial_ofensivo_base * 2
+    else
+      self.potencial_ofensivo_base
+    end
+    #lógica para duplicar potencial ofensivo al atacar
+  end
+
+  def descansar
+    self.descansado = true
   end
 end
 
 class Guerrero
-  include UnidadDefensiva
   include UnidadOfensiva
+  alias_method :descansar_atacante, :descansar
 
-  attr_accessor :potencial_ofensivo
+  include UnidadDefensiva
+  alias_method :descansar_defensor, :descansar
+
+  attr_accessor :potencial_ofensivo_base, :peloton
 
   def initialize(energia=100, potencial_ofensivo=20, potencial_defensivo=10)
-    self.potencial_ofensivo = potencial_ofensivo
+    self.potencial_ofensivo_base = potencial_ofensivo
     self.energia = energia
     self.potencial_defensivo = potencial_defensivo
   end
+
+  def descansar
+    self.descansar_atacante
+    self.descansar_defensor
+  end
+
+  def ser_atacado(potencial_ofensivo)
+    super
+    self.interesados.each{ |un_interesado| un_interesado.fue_atacado(self) }
+  end
+
+  def interesados
+    @interesados ||= []
+  end
+
+
 end
 
 class Espadachin < Guerrero
@@ -40,6 +95,20 @@ class Espadachin < Guerrero
     otro_guerrero.ser_atacado(self.potencial_ofensivo + arma.potencial_ofensivo)
   end
 
+end
+
+class Kamikaze
+  include UnidadDefensiva
+  include UnidadOfensiva
+
+  def atacar(otro_guerrero)
+    super
+    self.energia = 0
+  end
+
+  def potencial_ofensivo_base
+    250
+  end
 end
 
 class Espada
@@ -62,7 +131,76 @@ end
 class Misil
   include UnidadOfensiva
 
-  def potencial_ofensivo
+  def potencial_ofensivo_base
     1000
+  end
+end
+
+class Peloton
+
+  attr_accessor :accion, :retirado
+
+  def guerreros
+    @guerreros ||= []
+  end
+
+  def initialize(una_accion)
+    self.accion = una_accion
+  end
+
+  def self.crear_con(*guerreros, &accion)
+    peloton = Peloton.new accion
+
+    guerreros.each { |un_guerrero|
+      peloton.agregar(un_guerrero)
+    }
+
+    peloton
+  end
+
+  def self.descansador(*guerreros)
+    crear_con(*guerreros, &bloque_descansador)
+  end
+
+  def self.bloque_descansador
+    lambda {|peloton|
+      peloton.guerreros.select {|g| g.lastimado}.each {|g|
+        g.descansar
+      }
+    }
+  end
+
+  def self.cobarde(*guerreros)
+    crear_con(*guerreros, &bloque_cobarde)
+  end
+
+  def self.bloque_cobarde
+    lambda {|peloton|
+      peloton.retirado = true
+    }
+  end
+
+  def self.estrategico(*guerreros)
+    crear_con(*guerreros) do |peloton|
+      self.bloque_cobarde.call(peloton)
+      peloton.accion = self.bloque_descansador
+    end
+  end
+
+  def self.patotero(victima, guerreros)
+    crear_con(*guerreros) do |peloton|
+      peloton.guerreros.each { |un_guerrero|
+        un_guerrero.atacar(victima)
+      }
+    end
+  end
+
+  def agregar(un_guerrero)
+    self.guerreros << un_guerrero
+    un_guerrero.interesados << self
+  end
+
+  def fue_atacado(un_guerrero)
+    self.accion.call(self)
   end
 end
