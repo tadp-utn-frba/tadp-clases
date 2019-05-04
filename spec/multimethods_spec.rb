d00d86f4e82b937cc4ee27d37a804f394d76bb09
pablitar <pablitar@gmail.com>
2019-05-04 12:55:07 -0300

    Código de la clase

---
+++ spec/multimethods_spec.rb
@@ -0,0 +1,165 @@
+require "rspec"
+require_relative "../src/multimethods"
+
+
+describe "matches" do
+  it("deberia matchear el mismo tipo") do
+      un_bloque = PartialBlock.new([String]) do |s|
+        "mucho no importa"
+      end
+
+      expect(un_bloque.matches?("unString")).to be(true)
+  end
+
+  it("deberia matchear super tipo") do
+    un_bloque = PartialBlock.new([Object]) do |s|
+      "mucho no importa"
+    end
+
+    expect(un_bloque.matches?("unString")).to be(true)
+  end
+
+  it("deberia no matchear si no hay relación") do
+    un_bloque = PartialBlock.new([String]) do |s|
+      "mucho no importa"
+    end
+
+    expect(un_bloque.matches?(3)).to be(false)
+  end
+
+  it("deberia matchear con multiples parámetros") do
+    un_bloque = PartialBlock.new([String, String]) do |s1, s2|
+      "mucho no importa"
+    end
+
+    expect(un_bloque.matches?("s1", "s2")).to be(true)
+    expect(un_bloque.matches?("s1", 1)).to be(false)
+    expect(un_bloque.matches?(1, 3)).to be(false)
+  end
+end
+
+describe "call" do
+  helloBlock = PartialBlock.new([String]) do |who|
+    "Hello #{who}"
+  end
+
+  it("deberia ejecutar con el parámetro que le paso") do
+    expect(helloBlock.call("Juan")).to eq("Hello Juan")
+  end
+
+  it("deberia arrojar error cuando le paso un tipo que no corresponde") do
+    expect{helloBlock.call(1)}.to raise_error("El bloque no coincide con los argumentos")
+  end
+end
+
+describe "partial_def" do
+  class A
+    partial_def :concat, [String, String] do |s1,s2|
+      s1 + s2
+    end
+
+    partial_def :concat, [String, Integer] do |s1,n|
+      s1 * n
+    end
+
+    partial_def :concat, [Array] do |a|
+      a.join
+    end
+  end
+
+  it("deberia funcionar concat") do
+    expect(A.new.concat('hello', ' world')).to eq('hello world')
+    expect(A.new.concat('hello', 3)).to eq('hellohellohello')
+    expect(A.new.concat(['hello', ' world', '!'])).to eq('hello world!')
+
+      #A.new.concat('hello', 'world', '!') # Lanza una excepción!
+  end
+
+  class C
+    partial_def :blah, [String, String] do |s1,s2|
+      s1 + s2
+    end
+  end
+
+  it 'debería saber si responde a un mensaje' do
+    expect(C.new.respond_to? :blah).to be(true)
+  end
+
+  it 'debería saber si responde a un mensaje con tipos' do
+    expect(C.new.respond_to? :blah, false, [String, String]).to be(true)
+  end
+
+  it 'debería saber si no responde a un mensaje con tipos' do
+    expect(C.new.respond_to? :blah, false, [Integer, Integer]).to be(false)
+  end
+
+  class Soldado
+# ... implementación de soldado
+  end
+
+  class Tanque
+    def ataca_con_canion(objetivo)
+      "canion"
+    end
+
+    def ataca_con_ametralladora(objetivo)
+      "ametralladora"
+    end
+
+    partial_def :ataca_a, [Tanque] do |objetivo|
+      self.ataca_con_canion(objetivo)
+    end
+
+    partial_def :ataca_a, [Soldado] do |objetivo|
+      self.ataca_con_ametralladora(objetivo)
+    end
+  end
+
+  it "deberia funcionar con self" do
+    tanque = Tanque.new
+    soldado = Soldado.new
+
+    expect(tanque.ataca_a(soldado)).to eq("ametralladora")
+    expect(tanque.ataca_a(tanque)).to eq("canion")
+  end
+
+  class F
+    partial_def :formatear, [String, [:nombre, :direccion]] do |titulo, coso|
+      titulo + " | " + coso.nombre + ": " + coso.direccion
+    end
+    partial_def :formatear, [String, [:peso]] do |titulo, pesable|
+      titulo + " " + pesable.peso.to_s
+    end
+  end
+
+  class Lugar
+    attr_accessor :nombre, :direccion, :fotos
+    def initialize(nombre, direccion)
+      @nombre = nombre
+      @direccion = direccion
+    end
+  end
+
+  class Perro
+    attr_accessor :nombre, :edad, :peso
+    def initialize(peso = 20, nombre = "pepe", edad = 10)
+      @nombre = nombre
+      @edad = edad
+      @peso = peso
+    end
+  end
+
+  it "deberia soportar ducktyping" do
+    expect(
+        F.new.formatear("VISITE", Lugar.new("Obelisco", "Corrientes y 9 de Julio"))
+    ).to eq("VISITE | Obelisco: Corrientes y 9 de Julio")
+
+    expect(
+        F.new.formatear("Pesado", Perro.new(32))
+    ).to eq("Pesado 32")
+  end
+
+
+
+end
+
---
+++ src/multimethods.rb
@@ -0,0 +1,134 @@
+class PartialBlock
+  def initialize(types, &block)
+    unless block.arity == types.length
+      raise "El bloque debería tener la misma aridad que la lista de tipos"
+    end
+
+    @types = types
+    @block = block
+  end
+
+  def matches?(*args)
+    matches_types? args.map {|it| it.class }
+      # args.length == @types.length &&
+      #     args.zip(@types).all? do |par|
+      #       par[0].is_a? par[1]
+      #     end
+  end
+
+  def call(*args)
+    unless matches? *args
+      raise "El bloque no coincide con los argumentos"
+    end
+
+    @block.call(*args)
+  end
+
+  def call_with(instance, *args)
+    unless matches? *args
+      raise "El bloque no coincide con los argumentos"
+    end
+
+    instance.instance_exec(*args, &@block)
+  end
+
+  def distance_to(*args)
+    args.zip(@types).sum do |par|
+      if par[1].is_a? Array
+        0.5
+      else
+        par[0].class.ancestors.index(par[1])
+      end
+    end
+  end
+
+  def matches_types?(types)
+    types.length == @types.length &&
+        types.zip(@types).all? do |it|
+          matches_single_type(it)
+        end
+  end
+
+  def matches_single_type(par)
+    tipo_definido = par[1] #Por ejemplo, Object, String, [:a, :b]
+    tipo_recibido = par[0] #Por ejemplo, Integer, Numeric, Object, String
+    if tipo_definido.is_a? Array
+      tipo_definido.all? do |mensaje|
+        tipo_recibido.method_defined? mensaje
+      end
+    else
+      #Una clase o un modulo
+      tipo_recibido <= tipo_definido
+    end
+  end
+end
+
+class Multimethod
+  def definiciones
+    @definiciones ||= Hash.new
+  end
+
+  def agregar_definicion(types, block)
+    definiciones[types] = PartialBlock.new(types, &block)
+  end
+
+  def call(this, *args)
+    partial_block = definiciones.values
+        .filter { |it| it.matches? *args }
+        .min_by { |it| it.distance_to *args}
+
+    return this.method_missing(*args) if partial_block.nil?
+
+    partial_block.call_with(this, *args)
+  end
+
+  def matches_types?(types)
+    definiciones.values.any? do |it|
+      it.matches_types? types
+    end
+  end
+end
+
+class Module
+
+  def partial_def(nombre, types, &block)
+
+    unless multimethods.has_key? nombre
+      multimethods[nombre] = Multimethod.new
+    end
+
+    multimethods[nombre].agregar_definicion(types, block)
+
+    define_method(nombre) do |*args|
+      call_multimethod(nombre, *args)
+    end
+  end
+
+  def multimethods
+    @multimethods ||= Hash.new
+  end
+
+  def multimethod(nombre)
+    multimethods[nombre]
+  end
+end
+
+class Object
+  def call_multimethod(nombre, *args)
+    a_method = self.class.multimethod(nombre)
+
+    return method_missing(nombre, *args) if a_method.nil?
+
+    a_method.call(self, *args)
+  end
+
+  alias_method :__respond_to?, :respond_to?
+
+  def respond_to?(name, include_all = false, types = nil)
+    if types.nil?
+      __respond_to?(name, include_all)
+    else
+      self.class.multimethod(name).matches_types? types
+    end
+  end
+end
