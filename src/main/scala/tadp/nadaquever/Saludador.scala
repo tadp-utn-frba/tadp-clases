54c1c2bd4d2c1f60d7c6de57243735c2682c2a8c
MARIANACECILIAMATOS <matosm@ar.ibm.com>
2019-05-08 20:02:39 -0300

    Agrego el ejemplo del saludador que está en el script

---
+++ src/main/scala/tadp/nadaquever/Saludador.scala
@@ -0,0 +1,13 @@
+package tadp.nadaquever
+/*
+object Saludador extends App {
+  case class Persona(name: String)
+
+  class Saludador {
+    def saludar(alguien: Persona) =
+      "hola" + alguien.nombre
+  }
+
+  new Saludador().saludar(new Persona("asdds"))
+}
+*/
