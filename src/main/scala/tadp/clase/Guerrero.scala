ee73c94f6bd2bb7d30b4859863701746970e278c
Axel Bergh <abergh@despegar.com>
2019-05-08 20:10:32 -0300

    Prepare for class example

---
+++ src/main/scala/tadp/clase/Guerrero.scala
@@ -0,0 +1,13 @@
+package tadp.clase
+
+class Guerrero {
+
+  var energia: Int = 100
+  var potencialOfensivo: Int = 20
+  var potencialDefensivo: Int = 10
+
+  def atacarA(otro: Guerrero): Unit = {
+    ???
+  }
+
+}
