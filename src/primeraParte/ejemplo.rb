7692a039f88e47a6fe8d99238dbff1059f6baceb
facuhump <facundo.humphreys@medialab.la>
2024-04-19 20:03:55 -0300

    Solucion completa de primera parte

--- src/primeraParte/ejemplo.rb
+++ src/primeraParte/ejemplo.rb
@@ -7,7 +7,7 @@ def ir_de_compras_con(un_carrito)
   un_carrito.agregar_producto("harina", 15)
 end
 
-class Registrador_de_compras
+class Registrador_de_compras < BasicObject
   attr_reader :mensajes_recibidos
 
   def initialize(objeto_original)
@@ -15,12 +15,28 @@ class Registrador_de_compras
     @mensajes_recibidos = []
   end
 
-  private def method_missing(symbol, *args, &block)
+  def method_missing(symbol, *args, &block)
     @mensajes_recibidos << { mensaje: symbol, argumentos: args }
     @objeto_original.send(symbol, *args, &block)
+  end
+
+  def respond_to_missing?(symbol, include_private = false)
+    @objeto_original.respond_to?(symbol, include_private) # || super
   end
 end
 
 carrito = CarritoDeCompra.new
-ir_de_compras_con(carrito)
+registrador_de_mensajes = Registrador_de_compras.new(carrito)
+ir_de_compras_con(registrador_de_mensajes)
 
+puts registrador_de_mensajes.mensajes_recibidos
+
+# Respond_to?
+
+puts carrito.respond_to? :total_a_pagar
+puts registrador_de_mensajes.respond_to? :total_a_pagar
+
+# is_a?
+
+puts carrito.is_a? CarritoDeCompra
+puts registrador_de_mensajes.is_a? CarritoDeCompra
