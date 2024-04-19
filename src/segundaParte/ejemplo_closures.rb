2b26b40ac2fed55e541aba552415644060b8df10
facuhump <facundo.humphreys@medialab.la>
2024-04-19 20:23:24 -0300

    Ejempĺos de como llamar a los bloques

--- src/segundaParte/ejemplo_closures.rb
+++ src/segundaParte/ejemplo_closures.rb
@@ -15,10 +15,30 @@ imprimir_hola = Proc.new { puts "Hola" } # Si no lo llamo no se ejecuta
 # Tengo la posibilidad de que reciba un bloque por parametro
 # Todos los metodos en ruby reciben un bloque implicito
 
-def m1
+def m1(un_bloque)
 
 end
 
 m1 do
   puts "Hola" # este bloque nunca se esta ejecutando
+end
+
+# Fijarse que asi falla
+
+def m2
+  yield
+  # yield, de esta forma lo ejecuto pero puedo querer agarrar el bloque y ejecutarlo en otro momento. Eso lo hago con el &bloque, tengo una referencia al bloque
+  # La desventaja de yield es que solo lo puedo usar dentro de este mensaje, no se lo puedo pasar a nada
+end
+
+m2 do
+  puts "Hola"
+end
+
+def m3(&bloque)
+  bloque.call
+end
+
+m3 do
+  puts "Chau"
 end
