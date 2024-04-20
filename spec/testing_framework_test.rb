6a9ed32070e2276b2233fd14ca2cc7911493f3ea
facuhump <facundo.humphreys@medialab.la>
2024-04-19 23:35:09 -0300

    Agregandole nombres a los tests

--- spec/testing_framework_test.rb
+++ spec/testing_framework_test.rb
@@ -1,12 +1,10 @@
 require '../src/segundaParte/testing/testing_framework'
-require 'rainbow/refinement'
-using Rainbow
 
 assert false
 assert true
 
 se_han_ejecutado_los_tests = false # dentro del bloque tengo acceso a este booleano
-test_suite = TestSuite.new do
+test_suite = TestSuite.new'Quiero que falle' do
   #implicito esta self.test()
   test do
     se_han_ejecutado_los_tests = true
--- src/segundaParte/testing/testing_framework.rb
+++ src/segundaParte/testing/testing_framework.rb
@@ -1,10 +1,15 @@
+require 'rainbow/refinement'
+using Rainbow
+
 def assert(un_booleano)
   puts "FAIL".red if !un_booleano
   puts "PASS".green if un_booleano
 end
 
 class TestSuite
-  def initialize(&definicion_de_las_suite)
+  attr_accessor :nombre_del_test
+  def initialize(nombre_del_test,&definicion_de_las_suite)
+    @nombre_del_test = nombre_del_test
     instance_eval(&definicion_de_las_suite)
   end
 
