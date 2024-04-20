bbdd0c218df1e609437fdba47a7e16bcb84261cb
facuhump <facundo.humphreys@medialab.la>
2024-04-19 23:26:56 -0300

    Modelo basico de test framework

--- spec/testing_framework_test.rb
+++ spec/testing_framework_test.rb
@@ -1,2 +1,17 @@
 require '../src/segundaParte/testing/testing_framework'
+require 'rainbow/refinement'
+using Rainbow
 
+assert false
+assert true
+
+se_han_ejecutado_los_tests = false # dentro del bloque tengo acceso a este booleano
+test_suite = TestSuite.new do
+  #implicito esta self.test()
+  test do
+    se_han_ejecutado_los_tests = true
+  end
+end
+
+test_suite.ejecutar
+assert se_han_ejecutado_los_tests
--- src/segundaParte/testing/testing_framework.rb
+++ src/segundaParte/testing/testing_framework.rb
@@ -1,1 +1,18 @@
+def assert(un_booleano)
+  puts "FAIL".red if !un_booleano
+  puts "PASS".green if un_booleano
+end
 
+class TestSuite
+  def initialize(&definicion_de_las_suite)
+    instance_eval(&definicion_de_las_suite)
+  end
+
+  def test(&definicion_del_test)
+    definicion_del_test.call
+  end
+
+  def ejecutar
+
+  end
+end
