e4b87a827f2a9acfe342b87f8fc9da7577a01f2f
Juan Manuel Fernandes dos Santos <juan9794@gmail.com>
2025-06-07 07:41:59 -0300

    Actualizada version funcional-inmutable a scala 3

---
+++ funcional-inmutable/build.sbt
@@ -0,0 +1,12 @@
+
+organization := "ar.edu.utn.frba.tadp"
+
+name := "granja"
+
+version := "1.0"
+
+scalaVersion := "3.7.0"
+
+libraryDependencies ++= List(
+  "org.scalatest" %% "scalatest" % "3.2.19" % "test"
+)
--- funcional-inmutable/src/main/scala/ar/edu/utn/tadp/microprocesador/Operations3.scala
+++ funcional-inmutable/src/main/scala/ar/edu/utn/tadp/microprocesador/Operations3.scala
@@ -26,17 +26,17 @@ package object Operations3 {
   // ---------------------------------------------------------------------------------------------------------------------------------
   
   def run(program: Program, initialMicro: Micro): Result = {
-		  program.foldLeft(Result(initialMicro)) {
-		  case (previousResult: Halted, _)                            => previousResult
-		  case (previousResult: Failure, _)                           => previousResult
-		  case (Success(micro), Add)                                  => Result(micro.copy(a = micro.a + micro.b))
-		  case (Success(micro), Mul)                                  => Result(micro.copy(a = micro.a * micro.b))
-		  case (Success(micro), Swap)                                 => Result(micro.copy(a = micro.b, b = micro.a))
-		  case (Success(micro), Load(address))                        => Result(micro.copy(a = micro.mem(address)))
-		  case (Success(micro), Store(address))                       => Result(micro.copy(mem = micro.mem.updated(address, micro.a)))
-		  case (previousResult @ Success(micro), If(subInstructions)) => if (micro.a == 0) run(subInstructions, micro) else previousResult
-		  case (Success(micro), Halt)                                 => Halted(micro)
-		  }
+      program.foldLeft(Result(initialMicro)) {
+      case (previousResult: Halted, _)                            => previousResult
+      case (previousResult: Failure, _)                           => previousResult
+      case (Success(micro), Add)                                  => Result(micro.copy(a = micro.a + micro.b))
+      case (Success(micro), Mul)                                  => Result(micro.copy(a = micro.a * micro.b))
+      case (Success(micro), Swap)                                 => Result(micro.copy(a = micro.b, b = micro.a))
+      case (Success(micro), Load(address))                        => Result(micro.copy(a = micro.mem(address)))
+      case (Success(micro), Store(address))                       => Result(micro.copy(mem = micro.mem.updated(address, micro.a)))
+      case (previousResult @ Success(micro), If(subInstructions)) => if (micro.a == 0) run(subInstructions, micro) else previousResult
+      case (Success(micro), Halt)                                 => Halted(micro)
+    }
   }
 
 }
