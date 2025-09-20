e293eac7680f6c2b0097c91a0573333ca4e35c3b
juanFdS <juan9794@gmail.com>
2025-09-20 00:52:21 -0300

    2025 2c

--- spec/multimethods_spec.rb
+++ spec/multimethods_spec.rb
@@ -1,266 +1,97 @@
 require 'rspec'
 require_relative '../src/multimethods'
 
-describe "Partial Blocks" do
-  let(:helloBlock) do
-
-    PartialBlock.new([String]) do |who|
-      "Hello #{who}"
-    end
-
-  end
-
-  it "un partial block matchea con un parametro del tipo correcto" do
-    expect(helloBlock.matches?("a")).to be true
-  end
-
-  it "un partial block no matchea con un parametro del tipo incorrecto" do
-    expect(helloBlock.matches?(1)).to be false
-  end
-
-  it "un partial block no matchea si la cantidad de parametros no corresponde con la cantidad de tipos" do
-    expect(helloBlock.matches?("a", "b")).to be false
-  end
-
-  it "un partial block de multiples parametros matchea si cada parametro se corresponde con su tipo" do
-    mi_partial_block = PartialBlock.new([String, Integer]) do |who, howMany|
-      "Hello #{who}" * howMany
-    end
-
-    expect(mi_partial_block.matches?("a", 3)).to be true
-  end
-
-  it "un partial block de multiples parametros NO matchea si algun parametro no se corresponde con su tipo" do
-    mi_partial_block = PartialBlock.new([String, Integer]) do |who, howMany|
-      "Hello #{who}" * howMany
-    end
-
-    expect(mi_partial_block.matches?("a", "b")).to be false
-  end
-
-  it "el constructor tiene mas elementos que argumentos tiene el bloque proporcionado, falla" do
+RSpec.describe 'Multimethods' do
+  it 'crear un PartialBlock falla si tiene distinta cantidad de parametros en su bloque que en su lista de tipos' do
     expect do
-      PartialBlock.new([String]) do |who, what|
-        "Hello #{who}"
-      end
+      PartialBlock.new([]) { |who| "Hello #{who}" }
     end.to raise_error(ArgumentError)
   end
 
-  it "si lo evaluo con unos parametros, deberia darme el resultado del bloque" do
-    resultado = helloBlock.call("cualquier string")
+  it '....' do
+    hello_block = PartialBlock.new([String]) do |who|
+      "Hello #{who}"
+    end
 
-    expect(resultado).to eq("Hello cualquier string")
+    expect(hello_block.matches?("a")).to be true
+    expect(hello_block.matches?(1)).to be false
+    expect(hello_block.matches?("a", "b")).to be false
   end
 
-  it "si lo evaluo con parametros invalidos, deberia fallar con ArgumentError" do
-    expect {
-      helloBlock.call(5)
-    }.to raise_error(ArgumentError)
-  end
+  it "...call" do
+    hello_block = PartialBlock.new([String]) do |who|
+      "Hello #{who}"
+    end
 
-  it "el tipado de un partial block considera subtipos" do
+    expect(hello_block.call("world!")).to eq("Hello world!")
+    expect { hello_block.call(1) }.to raise_error(ArgumentError)
+
     pairBlock = PartialBlock.new([Object, Object]) do |left, right|
       [left, right]
     end
 
-    expect(pairBlock.call("hello", 1)).to eq(["hello", 1])
+    expect(pairBlock.call("hello", 1)).to eq ["hello", 1]
   end
 
-  # it "el tipado de un partial block considera modules" do
-  #   headBlock = PartialBlock.new([Enumerable]) do |enumerable|
-  #     enumerable.to_a[0]
-  #   end
-  #
-  #   expect(headBlock.call([1, 2, 3])).to eq(1)
-  #   expect(headBlock.call({ a: 1,
-  #                           b: "hundiste mi acarozado" })).to eq([:a, 1])
-  # end
-end
 
-describe "Multimethods" do
-  let(:class_a) do
-    Class.new do
-      partial_def :concat, [String, String] do |s1, s2|
-        s1 + s2
+  context 'multimethods' do
+    it 'con un solo partial def' do
+      klass = Class.new do
+        partial_def :concat, [String, String] do |s1,s2|
+          s1 + s2
+        end
       end
 
-      partial_def :concat, [String, Integer] do |s1, n|
-        s1 * n
-      end
-
-      partial_def :concat, [Array] do |a|
-        a.join
-      end
+      expect(klass.new.concat("a", "b")).to eq("ab")
+      expect { klass.new.concat("a", 1) }.to raise_error(NoMethodError)
     end
-  end
 
-  let(:class_b) do
-    Class.new do
-      partial_def :concat, [Object, Object] do |o1, o2|
-        "Objetos concatenados"
-      end
-
-      partial_def :concat, [String, Integer] do |s1, n|
-        s1 * n
-      end
-    end
-  end
-
-  it "casos base" do
-    un_a = class_a.new
-    expect(un_a.concat('hello', ' world'))
-      .to eq("hello world")
-
-    expect(un_a.concat('hello', 3)).to eq("hellohellohello")
-    expect(un_a.concat(['hello', ' world', '!'])).to eq("hello world!")
-    expect { un_a.concat('hello', 'world', '!') }
-      .to raise_error(ArgumentError)
-  end
-
-  it 'responde multimethods' do
-    expect(class_a.multimethods).to eq([:concat])
-  end
-
-  # it 'responde a multimethod' do
-  #   expect(class_a.multimethod(:concat)).not_to be_nil
-  # end
-
-  it "un multimethod ejecuta la definicion mas especifica que tenga" do
-    un_b = class_b.new
-
-    expect(un_b.concat("Hello", 2)).to eq("HelloHello")
-    expect(un_b.concat(Object.new, 3)).to eq("Objetos concatenados")
-  end
-
-  it "un objeto deberia poder responder un mensaje que tiene definido como multimethod" do
-    un_b = class_b.new
-
-    expect(un_b.respond_to?(:concat)).to be true
-  end
-
-  it "un objeto deberia poder responder un mensaje que tiene definido como multimethod si los parametros que le paso son correctos" do
-    un_b = class_b.new
-
-    expect(un_b.respond_to?(:concat, false, [String, String])).to be true
-  end
-
-  it "..." do
-    un_b = class_b.new
-
-    expect(un_b.respond_to?(:concat, false, [Integer, class_b])).to be true
-  end
-
-  it "si le paso tipos y lo entendia pero no por multimethods, da false el respond_to?" do
-    un_b = class_b.new
-
-    expect(un_b.respond_to?(:to_s, false, [String, String])).to be false
-  end
-
-  it "si los tipos no coinciden, da false el respond_to?" do
-    un_b = class_b.new
-
-    expect(un_b.respond_to?(:concat, false, [String, String, String])).to be false
-  end
-
-  let(:class_con_dos_multimethods) do
-    Class.new do
-      partial_def :concat, [Object, Object] do |o1, o2|
-        "Objetos concatenados"
-      end
-
-      partial_def :concat, [String, Integer] do |s1, n|
-        s1 * n
-      end
-
-      partial_def :saludar, [String] do |x|
-        "Hola #{x}"
-      end
-
-      partial_def :saludar, [String, Integer] do |x, n|
-        "Hola #{x}" + "!" * n
-      end
-    end
-  end
-
-  it "deberiamos poder tener mas de un multimethod" do
-    un_objeto_con_dos_multimethods = class_con_dos_multimethods.new
-
-    expect(un_objeto_con_dos_multimethods.concat("a", 3)).to eq("aaa")
-    expect(un_objeto_con_dos_multimethods.saludar("juan carlos", 3)).to eq("Hola juan carlos!!!")
-  end
-
-  describe "usando self" do
-    let(:class_c) do
+    let(:klass_a) do
       Class.new do
-        attr_accessor :energia
+        partial_def :concat, [String, String] do |s1,s2|
+          s1 + s2
+        end
 
-        def initialize
+        partial_def :concat, [String, Integer] do |s1,n|
+          s1 * n
+        end
+
+        partial_def :concat, [Array] do |a|
+          a.join
+        end
+      end
+    end
+
+    it "xx" do
+      expect(klass_a.new.concat('hello', ' world')). to eq('hello world')
+      expect(klass_a.new.concat('hello', 3)). to eq('hellohellohello')
+      expect(klass_a.new.concat(['hello', ' world', '!'])). to eq('hello world!')
+      expect { klass_a.new.concat('hello', 'world', '!') }. to raise_error(NoMethodError)
+    end
+
+    it "deberia ejecutarse en el contexto del receptor del mensaje" do
+      klass = Class.new do
+        attr_reader :energia
+        def initialize()
           @energia = 0
         end
 
-        partial_def :volar, [Integer] do |kms|
-          self.energia -= kms
+        partial_def :comer, [Integer] do |gramos|
+          @energia += gramos
         end
 
-        partial_def :volar, [] do
-          self.volar(10)
+        partial_def :comer, [Array] do |comidas|
+          comidas.each { |gramos| comer(gramos) }
         end
       end
+
+      pepita = klass.new
+      pepita.comer(2)
+      expect(pepita.energia).to eq 2
+
+      josefa = klass.new
+      josefa.comer([2, 3, 4, 5])
+      expect(josefa.energia).to eq 14
     end
-
-    it "los multimethods pueden referenciar a self" do
-      pepita = class_c.new
-
-      pepita.volar(10)
-
-      expect(pepita.energia).to eq(-10)
-    end
-  end
-
-  it "tp individual" do
-    klass_d = Class.new do
-      partial_def :formatear, [String, [:nombre, :direccion]] do |titulo, coso|
-        titulo + " | " + coso.nombre + ": " + coso.direccion
-      end
-      partial_def :formatear, [String, [:peso]] do |titulo, pesable|
-        titulo + " " + pesable.peso.to_s
-      end
-    end
-
-    klass_lugar = Class.new do
-      attr_accessor :nombre, :direccion, :fotos
-
-      def initialize(nombre, direccion)
-        @nombre = nombre
-        @direccion = direccion
-      end
-    end
-
-    klass_perro = Class.new do
-      attr_accessor :nombre, :edad, :peso
-
-      def initialize(peso)
-        @peso = peso
-      end
-    end
-
-    expect(
-      klass_d.new.formatear("VISITE", klass_lugar.new("Obelisco", "Corrientes y 9 de Julio"))
-    #devuelve
-    ).to eq("VISITE | Obelisco: Corrientes y 9 de Julio")
-
-    expect(
-      klass_d.new.formatear("Pesado", klass_perro.new(32))
-    ).to eq("Pesado 32")
-
-    expect(klass_d.new.respond_to?(:formatear, false, [String, [:peso]]))
-
-    expect {
-      klass_d.new.formatear("Pesado", 5)
-    }.to raise_error(ArgumentError)
-
   end
 end
-
-
-
--- src/multimethods.rb
+++ src/multimethods.rb
@@ -1,152 +1,55 @@
 class PartialBlock
-
-  attr_reader :tipos, :bloque
-
-  def initialize(lista_tipos, &block)
-    @tipos = lista_tipos
-    @bloque = block
-
-    raise ArgumentError unless lista_tipos.length == block.parameters.length
+  def initialize(types, &block)
+    @types = types
+    @block = block
+    raise ArgumentError.new if types.size != block.arity
   end
 
-  def matches?(*argumentos)
-    argumentos.length == tipos.length &&
-      tipos.zip(argumentos).all? do |tipo, argumento|
-        if tipo.is_a?(Array)
-          # [[:nombre, :direccion], Lugar.new]
-          tipo.all? do |mensaje|
-            argumento.respond_to?(mensaje)
-          end
-        else
-          # [[String, 'a']]
-          argumento.is_a? tipo
-        end
-
+  def matches?(*parameters)
+    parameters.size == @types.size &&
+      parameters.zip(@types).all? do |parameter, type|
+        matches_type?(parameter, type)
       end
   end
 
-  def matches_types?(*tipos_esperados)
-    tipos_esperados.length == tipos.length &&
-      tipos.zip(tipos_esperados).all? do |tipo, tipo_esperado|
-        if tipo.is_a?(Array)
-          tipo.all? do |mensaje|
-            tipo_esperado.instance_methods.include(mensaje)
-          end
-        else
-          tipo_esperado < tipo
-        end
-      end
-  end
-
-  def call(*argumentos)
-    raise ArgumentError.new "El bloque requiere #{tipos.length} argumentos" unless matches?(*argumentos)
-    bloque.call(*argumentos)
-  end
-
-  def evaluate_in(instancia, *argumentos)
-    instancia.instance_exec(*argumentos, &bloque)
-  end
-
-  def distance_to(*parametros)
-    parametros
-      .zip(@tipos, (1..parametros.length))
-      .sum do |parametro, tipo, indice|
-      if tipo.is_a?(Array)
-        0.5
-      else
-        indice * distance_for(parametro, tipo)
-      end
+  def matches_type?(parameter, type)
+    if type.is_a? Module
+      parameter.is_a? type
+    else
+      type.all? { |method_name| paramter.respond_to?(method_name) }
     end
   end
 
-  def distance_for(parametro, tipo)
-    parametro.class.ancestors.index(tipo)
+
+  def call(*parameters)
+    raise ArgumentError.new unless matches?(*parameters)
+
+    @block.call(*parameters)
+  end
+
+  def call_in_context(context, *parameters)
+    context.instance_exec(*parameters, &@block)
+  end
+end
+
+# partial_def :concat, [String, String] do |s1,s2|
+#   s1 + s2
+# end
+module WithPartialBlocks
+  attr_reader :multimethods
+  def partial_def(method_name, types, &block)
+    @multimethods ||= {}
+    @multimethods[method_name] ||= []
+    @multimethods[method_name].push(PartialBlock.new(types, &block))
+
+    define_method(method_name) do |*parameters|
+      partial_blocks = self.class.multimethods[method_name]
+      partial_block = partial_blocks.find(proc { raise NoMethodError.new }) { |partial_block| partial_block.matches?(*parameters) }
+      partial_block.call_in_context(self, *parameters)
+    end
   end
 end
 
 class Module
-  def partial_def(method_name, tipos, &block)
-    # agregando la definicion
-    partial_blocks = partial_blocks(method_name)
-    partial_blocks.append(PartialBlock.new(tipos, &block))
-
-    define_method method_name do |*args|
-      # tiempo de ejecucion
-      # FIXME: reificar a multimethod y delegar.
-      partial_block = partial_blocks.select do |partial_block|
-        partial_block.matches?(*args)
-      end.min_by do |partial_block|
-        partial_block.distance_to(*args)
-      end
-
-      raise ArgumentError if partial_block.nil?
-
-
-      partial_block.evaluate_in(self, *args)
-    end
-  end
-
-  def multimethods
-    (@partial_blocks || {}).keys
-  end
-
-  def multimethod(method_name)
-    partial_blocks(method_name)
-  end
-
-  def partial_blocks(method_name)
-    # lazy initialization
-    @partial_blocks ||= {}
-    @partial_blocks[method_name] ||= []
-  end
+  include WithPartialBlocks
 end
-
-class Object
-
-  def respond_to?(sym, include_all=false, tipos=nil)
-    return super(sym, include_all) if tipos.nil?
-
-    self.class.multimethod(sym).any? do |partial_block|
-      partial_block.matches_types?(*tipos)
-    end
-  end
-end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
