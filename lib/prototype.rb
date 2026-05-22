155b09123bc703fbee241e322902f9b826cba044
Ernesto Bossi <bossi.ernestog@gmail.com>
2026-05-21 21:22:36 -0300

    clase ruby disenio

--- lib/prototype.rb
+++ lib/prototype.rb
@@ -1,51 +1,43 @@
-class PropertyNotFound < Exception
-  attr_accessor :propiedad, :objeto
-
-  def initialize(propiedad, objeto)
-    self.propiedad = propiedad
-    self.objeto = objeto
-  end
-end
 class PrototypedObject
-  attr_accessor :propiedades, :prototipo
-  def initialize(prototipo = nil )
-    self.propiedades = {}
-    self.prototipo = prototipo
-  end
-  def copy()
-    PrototypedObject.new(self)
-  end
-  def set_property(nombre, valor)
-    self.propiedades[nombre] = valor
-  end
-  def get_property(nombre)
-    if self.propiedades.has_key? nombre
-      self.propiedades[nombre]
-    elsif self.prototipo.nil?
-      raise PropertyNotFound.new(nombre, self)
-    else
-      self.prototipo.get_property(nombre)
-    end
-  end
-  def has_property?(symbol)
-    self.propiedades.has_key?(symbol) || self.prototipo&.has_property?(symbol)
-  end
-  def respond_to_missing?(symbol, include_all)
-    self.has_property?(symbol.to_s.chomp("=").to_sym) || super
-  end
-  def method_missing(symbol, *args, **kargs, &block)
-    if symbol.to_s.end_with? "="
-      set_property(symbol.to_s.chomp("=").to_sym, args[0])
-    elsif self.has_property? symbol
-      propiedad = self.get_property(symbol)
-      if propiedad.is_a? Proc
-        instance_exec(*args, **kargs, &propiedad)
-      else
-        propiedad
-      end
+  def initialize
+    @__properties = {}
+    @__prototype
 
-    else
-      super
-    end
+    @__prototype_module = Module.new
+    self.extend @__prototype_module
+  end
+
+  def set_property(key, value)
+    @__properties[key] = value
+    set_method(key, proc{|| get_property(key)})
+  end
+
+  def get_property(key)
+    @__properties[key]
+  end
+
+  def set_method(method_sym, block)
+    @__prototype_module.define_method(
+      method_sym, &block)
+  end
+
+  def clone
+    shallow_copy = PrototypedObject.new
+    shallow_copy.
+      instance_variable_set(
+        :@__properties, self.
+        instance_variable_get(:@__properties).clone)
+    shallow_copy.set_prototype(self)
+    shallow_copy
+  end
+
+  def set_prototype(prototype)
+    @__prototype_module.
+      include(
+        prototype.
+          instance_variable_get(:@__prototype_module))
   end
 end
+
+
+
--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,5 +1,25 @@
 require_relative '../lib/prototype'
 
+def create_guerrero
+  guerrero = PrototypedObject.new
+  guerrero.set_property(:energia, 100)
+  guerrero.set_property(:potencial_ofensivo, 30)
+  guerrero.set_property(:potencial_defensivo, 10)
+
+  guerrero.set_method(:atacar_a, proc {
+      |otro_guerrero|
+    diff = self.potencial_ofensivo - otro_guerrero.potencial_defensivo
+    if diff > 0
+      otro_guerrero.recibir_danio(diff)
+    end
+  })
+
+  guerrero.set_method(:recibir_danio, proc {
+      |danio| set_property(:energia, [self.energia - danio, 0].max)
+  })
+  guerrero
+end
+
 describe 'Prototipos' do
   describe 'Un prototipo' do
     it 'deberia poder agregar una property' do
@@ -7,74 +27,62 @@ describe 'Prototipos' do
       guerrero.set_property(:sabiduria, 50)
 
       expect(guerrero.get_property(:sabiduria)).to eq 50
-      expect { PrototypedObject.new.get_property(:sabiduria) }
-        .to raise_error PropertyNotFound
+      expect(PrototypedObject.new
+                               .get_property(:sabiduria))
+        .to eq nil
     end
 
-    it 'deberia poder acceder a una property sin usar get_property' do
+    it "se pueden definir metodos con set_method" do
       guerrero = PrototypedObject.new
-      guerrero.set_property(:sabiduria, 50)
+      guerrero.set_method(:recibe_danio,
+                          proc do |danio|
+        nueva_energia = self.energia - danio
+        set_property(:energia, nueva_energia)
+      end)
 
-      expect(guerrero.sabiduria).to eq 50
-      expect { PrototypedObject.new.sabiduria }.to raise_error NoMethodError
+      guerrero.set_property(:energia, 100)
+      guerrero.recibe_danio(10)
+
+      expect(guerrero.energia).to eq 90
     end
 
-    it 'deberia poder saber qué mensajes sabe responder' do
-      guerrero = PrototypedObject.new
-      guerrero.set_property(:sabiduria, 50)
+    it "se pueden definir metodos con set_method" do
+      guerrero = create_guerrero
+      otro_guerrero = guerrero.clone
 
-      expect(guerrero.respond_to?(:sabiduria)).to be true
-      expect(guerrero.respond_to?(:insultar)).to be false
+      guerrero.atacar_a otro_guerrero
+      expect(otro_guerrero.energia).to eq 80
+      expect(guerrero.energia).to eq 100
     end
 
-    it 'deberia agregar un metodo' do
-      guerrero = PrototypedObject.new
-      guerrero.set_property(:sabiduria, 50)
+    it "un objeto prototipico puede usar los metodos de su prototipo" do
+      guerrero = create_guerrero
 
       espadachin = PrototypedObject.new
-      espadachin.set_property(:sabiduria, 0)
+      espadachin.set_property(:energia, 50)
+      espadachin.set_property(:habilidad, 0.5)
+      espadachin.set_property(:potencial_espada, 30)
+      espadachin.set_prototype(guerrero)
 
-      guerrero.set_property(:enseniar, proc {
-        |otro_guerrero| otro_guerrero.set_property(:sabiduria, self.sabiduria + otro_guerrero.sabiduria)
+      espadachin.set_method(:potencial_ofensivo, proc { ||
+        self.potencial_espada * self.habilidad
       })
 
-      guerrero.enseniar(espadachin)
-      expect(espadachin.sabiduria).to eq 50
+      espadachin.atacar_a(guerrero)
+      expect(guerrero.energia).to eq(95)
     end
 
-    it 'deberia poder crear prototipos' do
-      guerrero = PrototypedObject.new
-      guerrero.set_property(:sabiduria, 50)
-
-      espadachin = guerrero.copy
-
-      espadachin.set_property(:poder_espada, 100)
-
-      expect(espadachin.respond_to?(:poder_espada)).to be true
-      expect(espadachin.poder_espada).to eq 100
-      expect(guerrero.respond_to?(:poder_espada)).to be false
-
-      expect(espadachin.respond_to? :sabiduria).to be true
-
-      expect(espadachin.respond_to?(:no_method)).to be false
-    end
-
-    it 'deberia actualizar una property sin usar set_property' do
-      guerrero = PrototypedObject.new
-      guerrero.set_property(:sabiduria, 50)
-
-      guerrero.sabiduria = 30
-
-      espadachin = guerrero.copy
-
-      guerrero.set_property(:enseniar, proc {
-        |otro_guerrero| otro_guerrero.sabiduria = self.sabiduria + otro_guerrero.sabiduria
-      })
-
-      guerrero.enseniar(espadachin)
-
-      expect(guerrero.sabiduria).to eq 30
-      expect(espadachin.sabiduria). to eq 60
-    end
+    # it "multiples prototipos" do
+    #   guerrero = PrototypedObject.new
+    #   alquimista = PrototypedObject.new
+    #
+    #   guerrero.set_method(:hola, proc { "HOLA" })
+    #   alquimista.set_method(:hola, proc {"se te apetece alguna de mis pociones" })
+    #
+    #   coso = PrototypedObject.new
+    #   coso.set_prototypes([guerrero, alquimista])
+    #
+    #   expect(coso.hola).to eq "HOLA"
+    # end
   end
 end
